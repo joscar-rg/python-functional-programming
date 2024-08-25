a42e36ad8d5a556baed4058d8015f2843279850b
joscar-rg <40918848+joscar-rg@users.noreply.github.com>
2024-08-25 20:28:00 +0000

    [Ch 3] Combining list functions

--- Exercise Files/03_07/Start/03_07.py
+++ Exercise Files/03_07/Start/03_07.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 employees = [{
     'name': 'Jane',
     'salary': 90000,
@@ -23,3 +25,30 @@ employees = [{
     'salary': 70000,
     'job_title': 'marketing specialist'
 }]
+
+def is_developer(employee):
+    return employee['job_title'] == 'developer'
+
+def is_not_developer(employee):
+    return employee['job_title'] != 'developer'
+
+developers = list(filter(is_developer, employees))
+non_developers = list(filter(is_not_developer, employees))
+
+def get_salary(employee):
+    return employee['salary']
+
+developer_salaries = list(map(get_salary, developers))
+non_developer_salaries = list(map(get_salary, non_developers))
+
+def get_sum(acc, x):
+    return acc + x
+
+total_developer_salaries = reduce(get_sum, developer_salaries)
+average_developer_salary = total_developer_salaries / len(developer_salaries)
+
+total_non_developer_salaries = reduce(get_sum, non_developer_salaries)
+average_non_developer_salary = total_non_developer_salaries / len(non_developer_salaries)
+
+print(average_developer_salary)
+print(average_non_developer_salary)
