b1bb1b7a7c7c7421d3cc399069310f161019c4f3
joscar-rg <40918848+joscar-rg@users.noreply.github.com>
2024-08-25 20:09:26 +0000

    [Ch 3] Lambdas

--- Exercise Files/03_04/Start/03_04.py
+++ Exercise Files/03_04/Start/03_04.py
@@ -1,1 +1,13 @@
 numbers_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+add = lambda x, y: x + y
+print(add(2, 3))
+
+double_numbers = list(map(lambda x: x * 2, numbers_list))
+print(double_numbers)
+
+def create_multiplier(a):
+    return lambda x: x * a
+
+double = create_multiplier(2)
+print(double(5))
