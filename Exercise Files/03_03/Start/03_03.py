7ff48afa042c3a5ff5c355e05b8875c82da94fda
joscar-rg <40918848+joscar-rg@users.noreply.github.com>
2024-08-25 20:09:09 +0000

    [Ch 3] Filtering

--- Exercise Files/03_03/Start/03_03.py
+++ Exercise Files/03_03/Start/03_03.py
@@ -1,1 +1,7 @@
 numbers_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+def is_even(x):
+    return x % 2 == 0
+
+even_numbers_functional = list(filter(is_even, numbers_list))
+print(even_numbers_functional)
